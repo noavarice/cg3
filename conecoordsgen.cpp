85f75dd913a27c88b0369c01e0ea8bdfa124cf3b
noavarice <alex9razinkov@gmail.com>
2017-05-14 11:07:44 +0300

    fixed bug with array indexing

--- conecoordsgen.cpp
+++ conecoordsgen.cpp
@@ -19,15 +19,15 @@ void generateConeCoords(
     QVector3D currentPoint;
     QVector3D coneTop{0.0f, 0.0f, height};
     for (short i = 0; i < CIRCLE_POINTS_COUNT; ++i) {
-        resultArray[i] = Vertex(START_POINT);
-        resultArray[i + CIRCLE_POINTS_COUNT] = Vertex(coneTop);
+        resultArray[i * 3] = Vertex(START_POINT);
+        resultArray[(i + CIRCLE_POINTS_COUNT) * 3] = Vertex(coneTop);
 
         currentPoint = QVector3D(cos(currentAngle) * radius, sin(currentAngle) * radius, 0.0f);
-        resultArray[i + 1] = resultArray[i + CIRCLE_POINTS_COUNT + 1] = Vertex(currentPoint);
+        resultArray[i * 3 + 1] = resultArray[(i + CIRCLE_POINTS_COUNT) * 3 + 1] = Vertex(currentPoint);
 
         currentAngle += RADIAN_PART;
 
         currentPoint = QVector3D(cos(currentAngle) * radius, sin(currentAngle) * radius, 0.0f);
-        resultArray[i + 2] = resultArray[i + CIRCLE_POINTS_COUNT + 2] = Vertex(currentPoint);
+        resultArray[i * 3 + 2] = resultArray[(i + CIRCLE_POINTS_COUNT) * 3 + 2] = Vertex(currentPoint);
     }
 }
