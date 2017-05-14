8c22ec81975c90f5007238d75faae085cea1b9a5
noavarice <alex9razinkov@gmail.com>
2017-05-14 09:50:01 +0300

    test launch

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QSurfaceFormat>
+#include "openglwidget.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    QSurfaceFormat format;
+    format.setDepthBufferSize(24);
+    format.setStencilBufferSize(8);
+    format.setVersion(3, 2);
+    format.setProfile(QSurfaceFormat::CoreProfile);
+    QSurfaceFormat::setDefaultFormat(format);
+
+    OpenGLWidget w;
+    w.drawCone(1.0f, 1.0f);
     w.show();
 
     return a.exec();
