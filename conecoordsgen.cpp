9b085fd7d75b32a2f94be17d3c3d624026083558
noavarice <alex9razinkov@gmail.com>
2017-05-15 14:50:45 +0300

    compute normals for vertices

--- conecoordsgen.cpp
+++ conecoordsgen.cpp
@@ -2,6 +2,8 @@
 
 #include "vertex.h"
 
+#include <QPair>
+
 #define _USE_MATH_DEFINES
 #include <cmath>
 
@@ -9,25 +11,47 @@ static constexpr QVector3D START_POINT{0.0f, 0.0f, 0.0f};
 static constexpr short CIRCLE_POINTS_COUNT = 360;
 static constexpr float RADIAN_PART = 2 * M_PI / CIRCLE_POINTS_COUNT;
 
+static QVector3D getNormalVector(
+        const QVector3D& p1,
+        const QVector3D& p2,
+        const QVector3D& p3
+        )
+{
+    QVector3D u = p2 - p1;
+    QVector3D v = p3 - p1;
+    return {
+        u.y() * v.z() - u.z() * v.y(),
+        u.z() * v.x() - u.x() * v.z(),
+        u.x() * v.y() - u.y() * v.x(),
+    };
+}
+
+static QVector3D getVectorFromAngleAndRadius(float angle, float radius)
+{
+    return QVector3D(cos(angle) * radius, sin(angle) * radius, 0.0f);
+}
+
 void generateConeCoords(
         OUT Vertex* resultArray,
         float height,
         float radius
         )
 {
+    QPair<QVector3D, QVector3D> vertexPair;
+    QPair<QVector3D, QVector3D> normalPair;
     float currentAngle = 0.0f;
-    QVector3D currentPoint;
     QVector3D coneTop{0.0f, 0.0f, height};
     for (short i = 0; i < CIRCLE_POINTS_COUNT; ++i) {
-        resultArray[i * 3] = Vertex(START_POINT);
-        resultArray[(i + CIRCLE_POINTS_COUNT) * 3] = Vertex(coneTop);
-
-        currentPoint = QVector3D(cos(currentAngle) * radius, sin(currentAngle) * radius, 0.0f);
-        resultArray[i * 3 + 1] = resultArray[(i + CIRCLE_POINTS_COUNT) * 3 + 1] = Vertex(currentPoint);
-
+        vertexPair.first = getVectorFromAngleAndRadius(currentAngle, radius);
         currentAngle += RADIAN_PART;
+        vertexPair.second = getVectorFromAngleAndRadius(currentAngle, radius);
 
-        currentPoint = QVector3D(cos(currentAngle) * radius, sin(currentAngle) * radius, 0.0f);
-        resultArray[i * 3 + 2] = resultArray[(i + CIRCLE_POINTS_COUNT) * 3 + 2] = Vertex(currentPoint);
+        normalPair.first = getNormalVector(START_POINT, vertexPair.first, vertexPair.second);
+        normalPair.first = getNormalVector(coneTop, vertexPair.first, vertexPair.second);
+        resultArray[i * 3] = Vertex(START_POINT, normalPair.first);
+        resultArray[(i + CIRCLE_POINTS_COUNT) * 3] = Vertex(coneTop, normalPair.second);
+
+        resultArray[i * 3 + 1] = resultArray[(i + CIRCLE_POINTS_COUNT) * 3 + 1] = Vertex(vertexPair.first, normalPair.first);
+        resultArray[i * 3 + 2] = resultArray[(i + CIRCLE_POINTS_COUNT) * 3 + 2] = Vertex(vertexPair.second, normalPair.second);
     }
 }
